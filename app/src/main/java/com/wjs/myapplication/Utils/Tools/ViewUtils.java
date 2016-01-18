a83e9518782cf7a216bd4733f94bb4e764fa891d
shangwfa <shangwfa@126.com>
2016-01-18 11:26:35 +0800

    ViewUtils 增加功能性函数

--- app/src/main/java/com/wjs/myapplication/Utils/Tools/ViewUtils.java
+++ app/src/main/java/com/wjs/myapplication/Utils/Tools/ViewUtils.java
@@ -1,32 +1,106 @@
 package com.wjs.myapplication.Utils.Tools;
 
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
 
 
 /**
  * View工具类
- * 
+ * <p/>
  * ViewUtils.java
- * 
- * @author 火蚁(http://my.oschina.net/u/253900)
  *
+ * @author 火蚁(http://my.oschina.net/u/253900)
  * @data 2015-3-11 上午11:43:58
  */
 public class ViewUtils {
-    
+
     /***
      * 设置TextView的划线状态
-     * @author 火蚁
-     * 2015-3-11 上午11:46:10
      *
-     * @return void
      * @param tv
      * @param
+     * @return void
+     * @author 火蚁
+     * 2015-3-11 上午11:46:10
      */
-    public static void setTextViewLineFlag( TextView tv, int flags) {
-	tv.getPaint().setFlags(flags);
-	tv.invalidate();
+    public static void setTextViewLineFlag(TextView tv, int flags) {
+        tv.getPaint().setFlags(flags);
+        tv.invalidate();
     }
+
+    /**
+     * 功能：设置动态Viewpager高度
+     */
+    public static void setViewPagerHeight(ViewPager viewPager) {
+        ViewGroup viewGroup = (ViewGroup) viewPager.getParent();
+        if (viewGroup == null) {
+            return;
+        }
+        int viewPagerIndex = viewGroup.indexOfChild(viewPager);
+        int childViewHeight = getViewPagerHeight(viewPager); //获取ViewPager的子View的高度。
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, childViewHeight);//这里设置params的高度。
+        viewGroup.removeView(viewPager);
+        viewGroup.addView(viewPager, viewPagerIndex, params);//使用这个params
+    }
+
+    /**
+     * 功能：获取Viewpager子View的高度
+     */
+    public static int getViewPagerHeight(ViewPager viewPager) {
+        int childcount = viewPager.getChildCount();
+        int viewpagerHeight = 0;
+        for (int i = 0; i < childcount; i++) {
+            int h = getViewHeight(viewPager.getChildAt(i));
+            if (h > viewpagerHeight) {
+                viewpagerHeight = h;
+            }
+        }
+        return viewpagerHeight;
+    }
+
+    /**
+     * 功能：测量View的宽度
+     */
+    public static int getViewWidth(View view) {
+        int w = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        int h = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        view.measure(w, h);
+        return view.getMeasuredWidth();
+    }
+
+    /**
+     * 功能：测量View的宽度
+     */
+    public static int[] getViewWidthAndHeight(final View view) {
+        final int[] wh = new int[2];
+        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                wh[1] = view.getHeight();
+                wh[0] = view.getWidth();
+            }
+        });
+        return wh;
+    }
+
+    /**
+     * 功能：测量View的高度
+     */
+    public static int getViewHeight(View view) {
+        int w = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        int h = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        view.measure(w, h);
+        return view.getMeasuredHeight();
+    }
+
 }
 
---
+++ app/src/main/java/com/wjs/myapplication/view/CommonViewPager.java
@@ -0,0 +1,36 @@
+package com.wjs.myapplication.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by admin on 2016/1/18.
+ * 功能：自适应高度
+ */
+public class CommonViewPager extends ViewPager {
+    public CommonViewPager(Context context) {
+        super(context);
+    }
+
+    public CommonViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
